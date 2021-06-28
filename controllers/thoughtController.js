6d8c5da5c378de9821517f23726cb6f1ea8beb5a
Breedlove13 <brianbreedlove0013@gmail.com>
2021-06-28 10:17:03 -0700

    basics being done

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,7 @@ const { Thought } = require('../models')
 const thoughtController = {
   
     getAllThought(req, res) {
-    User.find({})
+    Thought.find({})
       .sort({ _id: -1 })
       .selected("_v")
       .then((dbThought) => res.json(dbThought))
@@ -14,11 +14,11 @@ const thoughtController = {
   },
 
   getThoughtById({ params }, res) {
-    User.findOne({ _id: params.id }) 
+    Thought.findOne({ _id: params.id }) 
       .select("-__v")
       .then((dbThought) => {
         if (!dbThought) {
-          res.status(404).json({ message: " No User Found" });
+          res.status(404).json({ message: " No thought Found" });
           return;
         }
         res.json(dbThought);
@@ -30,7 +30,7 @@ const thoughtController = {
   },
 
   createThought({ body }, res) {
-    User.create(body)
+    Thought.create(body)
       .then((dbThought) => res.json(dbThought))
       .catch((err) => res.status(400).json(err));
   },
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
 const router = require("express").Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes)
+
+router.use((req,res) => {
+    res.status(404).json({Message:'This route is not asssociated with us'})
+})
+
+module.exports = Router;
