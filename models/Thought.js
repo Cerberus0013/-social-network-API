ffbaf3c960a5d6fbbe39001919ea7fe183314a2b
Breedlove13 <brianbreedlove0013@gmail.com>
2021-06-28 08:20:38 -0700

    thought model done

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,28 @@
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+
+  username: {
+      type: String,
+      required: true,
+      trime: true
+  },
+reactions: [ReactionSchema]
+
+}
+
+);
+
 reactionSchema;
