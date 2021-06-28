429ef278c7fbf053a8403e11025445d039bdf852
Breedlove13 <brianbreedlove0013@gmail.com>
2021-06-28 08:08:59 -0700

    User model set up

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const Userschema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: User,
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+Userschema.virtual('friendCount').get(function (){
+    return this.friends.length
+})
+
+const User = model('User, Userschema')
+
+module.exports = User;
